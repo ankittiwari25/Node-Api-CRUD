675c313a9bddf6c53eac900e06c92da6fc34a3d3
ankitentigrity <ankit.tiwari@entigirty.com>
2023-12-29 18:25:49 +0530

    CRUD operation done with search and cron job

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const CronJobModel = require("./model/cron_job_model");
 const createCronJob = require('./config/cron-job/add_record_in_every_1_min');
 const port=3000;
 
-app.get('/',(req,res)=>{
-    res.send("This is simple route");
+app.get('/', (req, res) => {
+    const htmlContent =
+      "<html><body><h1>Terms and Conditions</h1><p>This is your terms and conditions content.</p></body></html>";
+    res.send(htmlContent);
+    // res.send("This is simple route");
 });
 
 app.listen(port,()=>{
