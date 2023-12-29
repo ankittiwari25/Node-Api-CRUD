3e8d0089f9448fdd6ee37fdb5a089a9c7ec6789b
ankitentigrity <ankit.tiwari@entigirty.com>
2023-12-29 18:28:53 +0530

    CRUD operation done with search and cron job

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const CronJobModel = require("./model/cron_job_model");
 const createCronJob = require('./config/cron-job/add_record_in_every_1_min');
 const port=3000;
 
-app.get('/', (req, res) => {
-    const htmlContent =
-      "<html><body><h1>Terms and Conditions</h1><p>This is your terms and conditions content.</p></body></html>";
-    res.send(htmlContent);
-    // res.send("This is simple route");
+app.get('/',(req,res)=>{
+    res.send("This is simple route");
 });
+
+ function getTerm(rer,res) {
+     res.send("fknmfnfnmfdnmdnf");
+}
 
 app.listen(port,()=>{
     console.log('Server is running on the port http://localhost:'+port);
