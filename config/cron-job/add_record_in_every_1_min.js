b9e9649da7397f26876c31eeeb4ba80a73a97202
ankitentigrity <ankit.tiwari@entigirty.com>
2023-12-29 16:59:15 +0530

    CRUD operation done with search and cron job

--- config/cron-job/add_record_in_every_1_min.js
+++ config/cron-job/add_record_in_every_1_min.js
@@ -1,21 +1,21 @@
 
-const CronJobService = require("/Users/chintanpandya/Ankit/php_practice/node js project with flutter app/services/cron.service.js");
+// const CronJobService = require("/Users/chintanpandya/Ankit/php_practice/node js project with flutter app/services/cron.service.js");
 const cron = require("node-cron");
 
 
 cron.schedule("* * * * *", async () => {
     try {
-    const firstName = "Ankit";
-    const middleName = "Vijaynath";
-    const lastName = "Tiwari";
-    const currentDate = new Date();
+    // const firstName = "Ankit";
+    // const middleName = "Vijaynath";
+    // const lastName = "Tiwari";
+    // const currentDate = new Date();
 
-    let cronJob = await CronJobService.createCronJobData({
-      firstName,
-      middleName,
-      lastName,
-      currentDate,
-    });
+    // let cronJob = await CronJobService.createCronJobData({
+    //   firstName,
+    //   middleName,
+    //   lastName,
+    //   currentDate,
+    // });
     console.log("cron job data added successfully => " +cronJob);
   } catch (error) {
     console.log("error is =>" + typeof error); 
